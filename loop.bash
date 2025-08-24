eabc7695b0a2d2750638ebdae92c1b34f479eb40
Michel Bolzon <michel.bolzon123@gmail.com>
2025-08-24 19:08:51 -0300

    Change sh to bash

---
+++ loop.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true
+do
+  php /var/www/tibia-hunteds-site/artisan world-scraper
+  sleep 3
+done
--- loop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while true
-do
-  php artisan world-scraper
-  sleep 3
-done
