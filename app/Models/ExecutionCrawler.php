22ccaa4816cfc83f35bf8ce1f4b36356f0c0d92c
Michel Bolzon <michel.bolzon123@gmail.com>
2025-09-03 22:30:16 -0300

    Create model for execution Crawler

---
+++ app/Models/ExecutionCrawler.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $guild_name
+ * @property string $url
+ * @property int $qtd_characters
+ * @property int $qtd_character_online
+ * @property int $qtd_character_offline
+ * @property int $execution_time
+ */
+class ExecutionCrawler extends Model {
+
+}
