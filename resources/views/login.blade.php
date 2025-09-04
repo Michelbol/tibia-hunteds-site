5c9203cbc0e99a4c09276084e1a32af167d901a5
Michel Bolzon <michel.bolzon123@gmail.com>
2025-09-03 22:18:54 -0300

    Improve Login Page view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="pt-BR">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Tibia Tracker</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 <body>
-<div>
 
-@error('email')
-    <div>{{ $message }}</div>
-@enderror
+<div class="login-container">
+    <h2>Login Guild Observer</h2>
+    @error('email')
+    <div id="errorBox" class="alert"> {{ $message }}</div>
+    @enderror
+    <form action="/login" method="POST">
+        @csrf
+        <input type="text" id="email" name="email" placeholder="Usuário" required>
+        <input type="password" id="password" name="password" placeholder="Senha" required>
+        <button type="submit">Entrar</button>
+    </form>
 </div>
-<form action="/login" method="POST">
-    @csrf
-    <input name="email">
-    <input name="password" type="password">
-    <button type="submit">Submit</button>
-</form>
+
 </body>
 </html>
