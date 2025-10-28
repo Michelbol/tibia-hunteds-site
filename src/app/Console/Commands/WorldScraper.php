bfb52c92371573258b63965277fe17d4fbbcb245
Michel Bolzon <michel.bolzon123@gmail.com>
2025-10-28 09:57:38 -0300

    Log execution crawler after everyting

--- src/app/Console/Commands/WorldScraper.php
+++ src/app/Console/Commands/WorldScraper.php
@@ -40,23 +40,47 @@ class WorldScraper extends Command {
 
             $globalExecutionEnd = microtime(true);
             $executionTime = $globalExecutionEnd - $globalExecutionBegin;
-            $this->createExecutionCrawler($searchGuild, $url, $guildPage, $executionTime);
 
             $this->info('Guild Page Scraped Summary: '.Carbon::now()->toDateTimeString());
             $this->info('Total Characters: '. $guildPage->getOnlineCharacters()+$guildPage->getOfflineCharacters());
         } catch (\Exception $e) {
             $this->info('Error to execute: '.Carbon::now()->toDateTimeString());
             Log::info($e->getMessage());
+        } finally {
+            $qtdTotal = 0;
+            $qtdOnline = 0;
+            $qtdOffline = 0;
+            if (is_null($searchGuild)) {
+                $searchGuild = 'Search didnt exists';
+            }
+            if (is_null($url)) {
+                $searchGuild = 'Url didnt exists';
+            }
+            if (!is_null($guildPage)) {
+                $qtdTotal = $guildPage->getOnlineCharacters()+$guildPage->getOfflineCharacters()
+                $qtdOnline = $guildPage->getOnlineCharacters();
+                $qtdOffline = $guildPage->getOfflineCharacters();
+            }
+            if (is_null($globalExecutionBegin)) {
+                $globalExecutionBegin = microtime(true);
+            }
+            if (is_null($globalExecutionEnd)) {
+                $globalExecutionEnd = microtime(true);
+            }
+            if (is_null($executionTime)) {
+                $executionTime = $globalExecutionEnd - $globalExecutionBegin;
+            }
+            $this->createExecutionCrawler($searchGuild, $url, $qtdTotal, $qtdOnline, $qtdOffline, $executionTime);
         }
     }
 
-    private function createExecutionCrawler(string $searchGuild, string $url, GuildPage $guildPage, float $executionTime): void {
+    private function createExecutionCrawler(string $searchGuild, string $url, int $qtdTtalCharacters, int $qtdOnline, int $qtdOffline, float $executionTime): void {
         $executionCrawler = new ExecutionCrawler();
         $executionCrawler->guild_name = $searchGuild;
         $executionCrawler->url = $url;
-        $executionCrawler->qtd_characters = $guildPage->getOnlineCharacters()+$guildPage->getOfflineCharacters();
-        $executionCrawler->qtd_character_online = $guildPage->getOnlineCharacters();
-        $executionCrawler->qtd_character_offline = $guildPage->getOfflineCharacters();
+        $executionCrawler->qtd_characters = $qtdTtalCharacters;
+        $executionCrawler->qtd_character_online = $qtdOnline;
+        $executionCrawler->qtd_character_offline = $qtdOffline;
         $executionCrawler->execution_time = $executionTime;
         $executionCrawler->scraping_time = $this->scrapTime;
         $executionCrawler->request_time = $this->requestTime;
