de0c18fdd4dc3d350cd6bfb86304246d56baf2d8
Michel Bolzon <michel.bolzon123@gmail.com>
2025-11-28 08:00:09 -0300

    Create cache for online players

--- src/app/Character/Repository/CharacterRepository.php
+++ src/app/Character/Repository/CharacterRepository.php
@@ -4,18 +4,29 @@ namespace App\Character\Repository;
 
 use App\Models\Character;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Storage;
 
 class CharacterRepository {
 
     public function getOnlinePlayer(?string $guildName = null): Collection {
+        $search = now()->subSeconds(2)->format('Y-m-d-H:i:s');
+        $cache = $this->getSearch("online-$search.json");
+        if (!is_null($cache) && !$cache->isEmpty()) {
+            return $cache;
+        }
+
         $query = Character::query()
         ->where('is_online', true);
 
         if (!is_null($guildName)) {
             $query->where('guild_name', $guildName);
         }
+        $result = $query->get();
+        $now = now()->format('Y-m-d-H:i:s');
 
-        return $query->get();
+        $this->saveSearch($now, $result);
+
+        return $result;
     }
 
     public function updateCharacterTypeUsingName(string $characterName, string $type): void {
@@ -40,5 +51,17 @@ class CharacterRepository {
 
     public function updateCharacterIsAttacker(string $characterName, bool $isAttacker): void {
         Character::where('name', $characterName)->update(['is_attacker_character' => $isAttacker]);
+    }
+
+    private function saveSearch(string $now, Collection $result): void {
+        Storage::disk('local')->put('online-' . $now . '.json', json_encode($result));
+    }
+
+    private function getSearch(string $fileName): ?Collection {
+        $cache = Storage::disk('local')->get($fileName);
+        if ($cache === false) {
+            return null;
+        }
+        return collect(json_decode($cache));
     }
 }
